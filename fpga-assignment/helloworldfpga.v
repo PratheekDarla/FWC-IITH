13601405945085b5d20836ae4299ad6c6024f079
PratheekDarla <root@localhost.localdomain>
2022-11-15 11:16:59 +0000

    FPGA assignment

---
+++ fpga-assignment/helloworldfpga.v
@@ -0,0 +1,15 @@
+module helloworldfpga(
+
+	input wire A,
+	input wire B,
+	input wire C,
+	input wire D,
+
+	output wire F,
+	);
+	
+	always @(*)
+	begin
+        F=((B&C)|(A&B&(!D))|(!A)&(!C)&D);  	
+	end
+	endmodule
